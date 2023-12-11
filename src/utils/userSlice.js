156480141036c86224f1624f513c0d65d32cf168
Saurav <91719965+saurav2942@users.noreply.github.com>
2023-12-11 19:52:01 +0530

    Update userSlice.js

--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -4,7 +4,7 @@ const userSlice = createSlice({
   name: "user",
   initialState: {
     username: "saurav2942",
-    userData: {},
+    userData: null,
   },
   reducers: {
     searchUser: (state, action) => {
