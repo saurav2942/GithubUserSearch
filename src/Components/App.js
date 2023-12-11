b5ecf034e2114ef4ba0dd43b6b475b45798c3c19
Saurav <91719965+saurav2942@users.noreply.github.com>
2023-12-11 20:07:04 +0530

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,16 @@ import Search from "./Search";
 import Body from "./Body";
 import Header from "./Header";
 import Shimmer from "./Shimmer";
+import {useState} from "react";
 import { useSelector } from "react-redux";
 
 const App = () => {
   const bgTheme = useSelector((store) => store.theme.bgTheme);
   const userData = useSelector((store) => store.user.userData);
+  const [fl, setFlag] = useState(userData!==null);
 
-  console.log(userData);
-  return (!userData?<Shimmer/>:(    
+  console.log(fl);
+  return (!fl?<Shimmer/>:(    
     <div
       className="box-border min-h-screen min-w-screen md:justify-center flex flex-col py-[2%] px-[2%] md:px-[10%] lg:px-[23%]"
       style={{ backgroundColor: bgTheme }}
